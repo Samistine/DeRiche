d68b1a28550050e5d9a6f771658f7a860ec4bc83
Samistine <samuel@samistine.com>
2017-09-05 20:59:07 -0400

    PHP: Patient Class
    To update your database do php bin/console doctrine:schema:update --force

---
+++ DeRiche/src/AppBundle/Entity/Patient.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Patient
+ *
+ * @ORM\Table(name="patient")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PatientRepository")
+ */
+class Patient
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=255, nullable=false)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255, nullable=false)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="insurance", type="string", length=255, nullable=false)
+     */
+    private $insurance;
+
+    /**
+     * Set id
+     *
+     * @param string $id
+     *
+     * @return Patient
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Patient
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Patient
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set insurance
+     *
+     * @param string $insurance
+     *
+     * @return Patient
+     */
+    public function setInsurance($insurance)
+    {
+        $this->insurance = $insurance;
+
+        return $this;
+    }
+
+    /**
+     * Get insurance
+     *
+     * @return string
+     */
+    public function getInsurance()
+    {
+        return $this->insurance;
+    }
+}
+
