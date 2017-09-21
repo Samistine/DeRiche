b637a0d9729d879935ade47d2d4beddc2275feaf
Samistine <samuel@samistine.com>
2017-09-21 19:47:12 -0400

    Patients

--- DeRiche/src/AppBundle/Controller/PatientsController.php
+++ DeRiche/src/AppBundle/Controller/PatientsController.php
@@ -16,12 +16,51 @@ class PatientsController extends Controller
      */
     public function indexAction()
     {
-        $patient = $this->getDoctrine()
+        $patients = $this->getDoctrine()
             ->getRepository(Patient::class)
             ->findAll();
 
         return $this->render('patients.html.twig', array(
-            'patients' => $patient
+            'patients' => $patients
         ));
+    }
+
+    /**
+     * @Route("/{id}/", name="View Patient")
+     */
+    public function viewPatient($id)
+    {
+        $patient = $this->getDoctrine()
+            ->getRepository(Patient::class)
+            ->find($id);
+
+        if (!$patient) {
+            throw $this->createNotFoundException(
+                'No patient found for id ' . $id
+            );
+        }
+
+        return $this->render('patient.html.twig', array('patient' => $patient));
+    }
+
+    /**
+     * @Route("createTestData", name="Create Test Patient")
+     */
+    public function createTestDate()
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $patient = new Patient();
+            $patient
+                ->setId(random_int(0, 999999999999999))
+                ->setFirstName(uniqid())
+                ->setLastName(uniqid())
+                ->setInsurance("No Insurance");
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($patient);
+            $em->flush();
+        }
+
+        return new Response("Test patients have been created");
     }
 }
