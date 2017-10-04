ef8d820bf85055261c4f91f1fd4d8d5c752856a3
Syed A <syvolt@gmail.com>
2017-10-03 20:00:29 -0400

    Beginning of users and roles.
    Will finish up at home. Should not break anything and can be pulled as is.

---
+++ DeRiche/src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sahmed6
+ * Date: 10/3/2017
+ * Time: 6:47 PM
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="Login Form")
+     */
+    public function loginAction(Request $request, AuthenticationUtils $authUtils)
+    {
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+    /**
+     * @Route("/admin", name="Admin Panel")
+     */
+    public function adminAction()
+    {
+        return $this->render('admin/admin.html.twig', array(
+        ));
+    }
+    /**
+     * @Route("/admin/create", name="Create Account")
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('replace_with_some_route');
+        }
+
+        return $this->render(
+            'admin/create.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+}
