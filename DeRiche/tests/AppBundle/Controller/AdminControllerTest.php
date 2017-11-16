d124b0f17ad3d492db71e64bd610a98094bbd3ed
Syed A <syvolt@gmail.com>
2017-11-16 17:26:01 -0500

    New Test
    Tests Admin Controller

--- DeRiche/tests/AppBundle/Controller/AdminControllerTest.php
+++ DeRiche/tests/AppBundle/Controller/AdminControllerTest.php
@@ -7,7 +7,7 @@ use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
-class LoginControllerTest extends WebTestCase
+class AdminControllerTest extends WebTestCase
 {
     private $client = null;
 
@@ -16,9 +16,9 @@ class LoginControllerTest extends WebTestCase
         $this->client = static::createClient();
     }
 
-    public function testAdmin()
+    public function testIndex()
     {
-        $this->logIn('ROLE_ADMIN');
+        $this->logIn();
         $crawler = $this->client->request('GET', '/admin/');
 
         // This test verifies whether the logged in user can access the /admin without a 302
@@ -27,34 +27,23 @@ class LoginControllerTest extends WebTestCase
         $this->assertSame('Admin Dashboard', $crawler->filter('h2')->text());
     }
 
-    public function testAuthor()
+    public function testCreatePage()
     {
-        $this->logIn('ROLE_WRITER');
-        $crawler = $this->client->request('GET', '/note/');
+        $this->logIn();
+        $crawler = $this->client->request('GET', '/admin/#create');
 
         // This test verifies whether the logged in user can access the /admin without a 302
         // It also verifies that the "Admin Dashboard" text can be verified.
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-        $this->assertSame('Type in Individual\'s Name', $crawler->filter('h5')->text());
+        $this->assertContains('Below is the form for creating users. Can only be accessed by the Administrator Role.', $this->client->getResponse()->getContent());
     }
 
-    public function testReviewer()
-    {
-        $this->logIn('ROLE_REVIEWER');
-        $crawler = $this->client->request('GET', '/reviewer/');
-
-        // This test verifies whether the logged in user can access the /admin without a 302
-        // It also verifies that the "Admin Dashboard" text can be verified.
-        $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-        $this->assertSame('Notes Awaiting Approval', $crawler->filter('h2')->text());
-    }
-
-    private function logIn($role)
+    private function logIn()
     {
         $session = $this->client->getContainer()->get('session');
         $firewallContext = 'main';
         // Create a test "user" token that has the role of admin so that it can view the correct page.
-        $token = new UsernamePasswordToken('admin', 'test', $firewallContext, array($role));
+        $token = new UsernamePasswordToken('admin', 'test', $firewallContext, array('ROLE_ADMIN', 'ROLE_USER'));
         $session->set('_security_'.$firewallContext, serialize($token));
         $session->save();
 
