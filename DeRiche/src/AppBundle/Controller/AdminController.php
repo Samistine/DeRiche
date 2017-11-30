87d7d12880dc6d435bfb16741b234f537cc0d611
Syed A <syvolt@gmail.com>
2017-11-30 18:40:25 -0500

    Note adding.
    Start of comments for sam to test phpdoc

--- DeRiche/src/AppBundle/Controller/AdminController.php
+++ DeRiche/src/AppBundle/Controller/AdminController.php
@@ -14,32 +14,36 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
+ * Main class for the Administrator Panel. We then extend the /admin/ route.
  * @Route("/admin/")
  */
 class AdminController extends Controller
 {
 
     /**
+     * Main panel for admins. This is the initial route that the /admin link will touch.
+     * It shows a panel of Create/View that Administrators can use for creating other users/admins.
      * @Route(name="Admin Panel")
      */
     public function indexAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
+        // We generate a blank user to get the fields we need for the creation form for other admins.
         $user = new User();
+        // The form is then generated.
         $form = $this->createForm(UserType::class, $user);
-
+        // This is where we handle the submitted form.
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // 3) Encode the password (you could also do this via Doctrine listener)
+            // We encode the password.
             $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
-
+            // We then submit the newly created user to the database.
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
         }
-
+        // The index panel also needs a list of Admins as we provide this to them to take action on.
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
-
         return $this->render('admin/admin.html.twig', array(
             'users' => $users,
             'form' => $form->createView()
@@ -47,6 +51,7 @@ class AdminController extends Controller
     }
 
     /**
+     * This route is for archiving users that the Admin no longer wants to be able to login.
      * @Route("archive/{username}", name="Archive User")
      */
     public function archiveUser(User $user, EntityManagerInterface $em, Request $request)
@@ -55,6 +60,7 @@ class AdminController extends Controller
         if ($user == $this->getUser())
             throw new BadRequestHttpException('You can not archive yourself!');
 
+        // Change the user status and submit to DB.
         $user->setIsActive(false);
         $em->persist($user);
         $em->flush();
@@ -65,6 +71,8 @@ class AdminController extends Controller
     }
 
     /**
+     * This is for us deleting the user. There are checks in here to make sure we don't
+     * delete a user that has notes attached.
      * @Route("delete/{username}", name="Delete User")
      */
     public function deleteUser(User $user, EntityManagerInterface $em, Request $request)
@@ -73,14 +81,15 @@ class AdminController extends Controller
         if ($user == $this->getUser())
             throw new BadRequestHttpException('You can not delete yourself!');
 
-        // Prevent the deletion of a dependency of notes
+        // Prevent the deletion of a user that has created notes.
         if (!$user->getAuthoredNotes()->isEmpty())
             throw new ConflictHttpException('Can not delete a user that has authored notes!');
 
-        // Prevent the deletion of a dependency of notes
+        // Prevent the deletion of a user that has reviewed notes.
         if (!$user->getReviewedNotes()->isEmpty())
             throw new ConflictHttpException('Can not delete a user that has reviewed notes!');
 
+        // We then persist it in the database.
         $em->remove($user);
         $em->flush();
 
@@ -90,31 +99,35 @@ class AdminController extends Controller
     }
 
     /**
- * @Route("secondaryrole/{username}", name="Update Secondary Role for User")
- */
+     * This is for modifying the secondary role/assigning a temporary role to a user.
+     * This was requested by the customer - this allows you to change/add/delete a role.
+     * @Route("secondaryrole/{username}", name="Update Secondary Role for User")
+     */
     public function secondaryRole(User $user, EntityManagerInterface $em, Request $request)
     {
-        // Get the role and add it as third value (After ROLE_USER)
+        // We get the role parameter from the request.
         $role = $request->get('role');
 
+        // We then get the user's existing roles (By default, [0] is ROLE_USER, [1] is the primary role.
+        // That leaves [2] as the secondary role, which is the only one we're interested in.
         $roles = $user->getRoles();
         $roles[2] = $role;
-        // Ignore the above if it's none and just remove it from the array.
+        // If there was no role submitted that means the "None" option was selected so let's delete the [2]' role.
         if ($role == "None") {
             unset($roles[2]);
         }
+        // Then we set the roles and persist it in the DB.
         $user->setRoles($roles);
         $em->persist($user);
         $em->flush();
 
-        //Get the page the browser was on before coming here
+        //Get the page the browser was on and send em back.
         $referer = $request->headers->get('referer');
-
-        //Send them back
         return $this->redirect($referer);
     }
 
     /**
+     * An admin can use this to change the password for a user.
      * @Route("password/{username}", name="Update Password for User")
      */
     public function setPassword(User $user, EntityManagerInterface $em, Request $request, UserPasswordEncoderInterface $passwordEncoder)
--- DeRiche/src/AppBundle/Controller/AuditController.php
+++ DeRiche/src/AppBundle/Controller/AuditController.php
@@ -17,12 +17,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
+ * Main route for auditing tools, primarily, auditing by patient, daterange and doing a full backup.
  * @Route("/audit/")
  */
 class AuditController extends Controller
 {
 
     /**
+     * Main index for the audit panel which just loads the template.
      * @Route(name="Audit Panel")
      */
     public function indexAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
@@ -31,45 +33,57 @@ class AuditController extends Controller
     }
 
     /**
-     * @Route("patient", name="Audit By User")
+     * Route for when the admin wants to audit by patient.
+     * @Route("patient", name="Audit By Patient")
      */
     public function auditPatient(Request $request)
     {
+        // We get the name that the Admin wants to look up.
         $patientName = $request->get('name');
+        // We then separate the name into two so we can search by first and last name.
         $names = explode(' ', $patientName, 2);
         $givenName = $names[0];
         $familyName = isset($names[1]) ? $names[1] : null;
+        // The actual look up function.
         $patients = $this->getDoctrine()
             ->getRepository(Patient::class)
             ->findBy([
                 'firstName' => $givenName,
                 'lastName' => $familyName
             ]);
+        // We then make sure that only one patient has that name.
         $count = count($patients);
         if ($count === 0) {
+            // If a patient was not found with that name.
             throw $this->createNotFoundException('No individual found for ' . $patientName);
         }
         if ($count === 1) {
+            // If a patient was found then we render the template with the notes.
             return $this->render('admin/audit.html.twig', array('notes' => $patients[0]->getNotes(),
                 'patient' => $patients[0]->getFirstName() . ' ' . $patients[0]->getLastName()));
         }
     }
 
     /**
+     * This is for when an admin wants all notes in a certain time period.
+     * We use the Repository file for Notes and added a getBetweenDates function which makes this simpler.
      * @Route("dates", name="Audit By DateRange")
      */
     public function auditDates(Request $request)
     {
+        // We get the variables and convert them into Datetime objects.
         $startDate = \DateTime::createFromFormat('m/d/Y', $request->get('start'));
         $endDate = \DateTime::createFromFormat('m/d/Y', $request->get('end'));
-
+        // Find the notes between that date.
         $notes = $this->getDoctrine()
             ->getRepository(Note::class)
             ->getBetweenDates($startDate, $endDate);
+        // Return the template
         return $this->render('admin/audit.html.twig', array('notes' => $notes));
     }
 
     /**
+     * This is when
      * @Route("db-backup", name="Audit - Full Backup")
      */
     public function dbBackup(EntityManagerInterface $em, Request $request)
@@ -81,7 +95,7 @@ class AuditController extends Controller
             // Let's do a SQLite backup. For the dev environment (tested).
             $output = shell_exec("sqlite3 $database .dump");
 
-            $response = new RespAonse();
+            $response = new Response();
             $response->headers->set('Content-Type', 'text/plain');
             $response->headers->set('Content-disposition', 'attachment;filename=full_backup.sql');
             $response->setContent($output);
